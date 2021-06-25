90f1eac96180947d6aa7d5b9741ab2bdca9b8ee2
Belchingcultist <ja4161@gmail.com>
2021-06-24 22:42:32 -0400

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: theme => ({
+        'stairs': "url('assets/img/pexels-bence-lengyel-3280178.jpg')",
         'cone': "url('assets/img/pexels-redrecords-©️-2743739.jpg')",
+        'chair': "url('assets/img/pexels-pixabay-158229.jpg')",
+        'path': "url('assets/img/pexels-alessandro-oliverio-1472234.jpg')"
       })
     },
     fontFamily: {
-      'other': ["NebulousRegular", 'sans-serif']
+      'NebulousRegular': ["NebulousRegular", 'sans-serif'],
+      'Beon': ["Beon", 'sans-serif'],
+      'Neoneon': ["Neoneon", 'sans-serif']
     }
   },
   variants: {
